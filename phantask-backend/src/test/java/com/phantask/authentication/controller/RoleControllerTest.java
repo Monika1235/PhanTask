212f504565d362d16ed352114a5e88eea3b8ca95
Monika Pati <128965611+Monika1235@users.noreply.github.com>
2026-03-08 00:44:03 +0530

    Update RoleControllerTest.java

--- phantask-backend/src/test/java/com/phantask/authentication/controller/RoleControllerTest.java
+++ phantask-backend/src/test/java/com/phantask/authentication/controller/RoleControllerTest.java
@@ -22,9 +22,6 @@ import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -36,12 +33,14 @@ import com.phantask.authentication.security.JwtFilter;
 import com.phantask.authentication.security.JwtUtil;
 import com.phantask.config.TestSecurityConfig;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestPropertySource;
 
 /**
  * Unit tests for RoleController
  */
 @WebMvcTest(RoleController.class)
 @Import(TestSecurityConfig.class)
+@TestPropertySource(properties = "spring.main.web-application-type = servlet")
 class RoleControllerTest {
 
     @Autowired
