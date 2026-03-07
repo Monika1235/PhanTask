7f9d5a40e7efcaebf1393bcfb980023dbd162295
Monika Pati <128965611+Monika1235@users.noreply.github.com>
2026-03-07 23:21:54 +0530

    Update TestSecurityConfig.java

--- phantask-backend/src/test/java/com/phantask/config/TestSecurityConfig.java
+++ phantask-backend/src/test/java/com/phantask/config/TestSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.http.HttpStatus;
 
 @TestConfiguration
 @EnableMethodSecurity
