2359d3cfbab663aac66fb81182fabe8ba8665112
Monika Pati <128965611+Monika1235@users.noreply.github.com>
2026-03-08 20:13:34 +0530

    Update UserController.java

--- phantask-backend/src/main/java/com/phantask/authentication/controller/UserController.java
+++ phantask-backend/src/main/java/com/phantask/authentication/controller/UserController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.phantask.authentication.dto.AccountCreationResponse;
 import com.phantask.authentication.dto.AdminEditUserRequest;
